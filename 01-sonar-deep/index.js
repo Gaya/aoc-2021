88d0dfeb18761f81081e23ae632bb495f32c5782
Gaya Kessler <gaya@theclevernode.com>
2021-12-02 18:41:41 +0100

    refactor: better structure for the first day

--- 01-sonar-deep/index.js
+++ 01-sonar-deep/index.js
@@ -1,6 +1,7 @@
-const { readFileSplitLines } = require('../helpers');
+const { readFileSplitLines } = require('../00-helpers/helpers');
+
 const findTimesDecreased = require('./findTimesDecreased');
 
-const solution = findTimesDecreased(readFileSplitLines('input').map((i) => parseInt(i, 10)));
+const solution = findTimesDecreased(readFileSplitLines('./input').map((i) => parseInt(i, 10)));
 
 console.log(solution);
